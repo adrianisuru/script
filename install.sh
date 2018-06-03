e80dcd7e98765079ec7128e561af78eca1701227
Adrian Herath <adrianisuru@gmail.com>
2018-06-03 14:51:49 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,3 +16,31 @@ if [ ping -q -w 1 -c 1 `ip r | grep default | cut -d ' ' -f 3` > /dev/null ]; th
 else
    echo "no internet"
 fi
+
+Then...
+
+partition disks
+
+mkfs and mkswap
+
+pacstrap /mnt base base-devel
+
+genfstab -U /mnt >> /mnt/etc/fstab
+ln -sf /usr/share/zoneinfo/Region/City /etc/localtime
+hwclock --systohc
+
+Uncomment en_US.UTF-8 UTF-8 and other needed localizations in /etc/locale.gen, and generate them with: 
+locale-gen
+
+echo LANG=en_US.UTF-8 > /etc/locale.conf
+
+echo hostname > /etc/hostname
+
+echo -e "127.0.0.1	localhost\n::1		localhost" >> /etc/hosts
+
+pacman -S networkmanager
+systemctl enable NetworkManager.service
+
+pacman -S neovim
+ln -s /bin/nvim /bin/vim
+
